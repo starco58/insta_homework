c655cdd634a847463422077f5a6626e82704006c
Todd S <starco58@gmail.com>
2014-05-23 13:02:58 -0500

    Added all recommended association methods

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,9 @@
 class Photo < ActiveRecord::Base
 
   belongs_to :user
+
+  has_many :comments
+  has_many :favorites
+
+  has_many :fans, :through => :favorites, :source => :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :photos
+  has_many :favorites
+
+  has_many :favorite_photos, :through => :favorites, :source => :photo
 end
